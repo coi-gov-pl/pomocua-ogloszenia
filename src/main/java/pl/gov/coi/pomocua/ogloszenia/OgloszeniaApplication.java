1c42fff83faa459643048088d87142dada538c3b
Mariusz Wyszomierski <mariusz.wyszomierski@coi.gov.pl>
2022-03-02 11:51:34 +0100

    Initial commit

---
+++ src/main/java/pl/gov/coi/pomocua/ogloszenia/OgloszeniaApplication.java
@@ -0,0 +1,13 @@
+package pl.gov.coi.pomocua.ogloszenia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgloszeniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgloszeniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/gov/coi/pomocua/ogloszenia/OgloszeniaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gov.coi.pomocua.ogloszenia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgloszeniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
