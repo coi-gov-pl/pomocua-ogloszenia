6944398ee039111ad33b4637f7ad49760e5489de
Krzysztof Rzymkowski <krzysztof.rzymkowski@snowflake.com>
2022-03-03 14:38:50 +0100

    remove SecurityConfig debug

--- src/main/java/pl/gov/coi/pomocua/ogloszenia/configuration/SecurityConfig.java
+++ src/main/java/pl/gov/coi/pomocua/ogloszenia/configuration/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity()
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
