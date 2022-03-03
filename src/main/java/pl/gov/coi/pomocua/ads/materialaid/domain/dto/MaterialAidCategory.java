9ec1ab35d320506b4fb84ad2e535b798a5fba2a6
Kacprowicz Marzena <Marzena.Kacprowicz@coi.gov.pl>
2022-03-03 21:08:03 +0100

    POM-43 [B] Zaprojektować kontrakt API
    * Implement MaterialAidAdController & MaterialAidAdDto + validation.

---
+++ src/main/java/pl/gov/coi/pomocua/ads/materialaid/domain/dto/MaterialAidCategory.java
@@ -0,0 +1,11 @@
+package pl.gov.coi.pomocua.ads.materialaid.domain.dto;
+
+public enum MaterialAidCategory {
+    FOOD,
+    HOUSEHOLD_GOODS,
+    ELECTRONICS,
+    FOR_CHILDREN,
+    COSMETICS_AND_DOMESTIC_DETERGENTS,
+    FURNITURE,
+    CLOTHING
+}
