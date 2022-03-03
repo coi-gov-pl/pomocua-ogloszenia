1463855dbb6c8a7b9f93d226ad329e434e8ec70b
Hubert Lipinski <hubert.lipinski@snowflake.com>
2022-03-03 14:51:55 +0100

    [POM-42] Add city lookup dictionary

---
+++ src/main/java/pl/gov/coi/pomocua/ogloszenia/dictionaries/domain/CityRepository.java
@@ -0,0 +1,10 @@
+package pl.gov.coi.pomocua.ogloszenia.dictionaries.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CityRepository extends CrudRepository<City, Long> {
+
+    List<City> findByVoivodeshipAndCityStartsWith(String voivodeship, String city);
+}
