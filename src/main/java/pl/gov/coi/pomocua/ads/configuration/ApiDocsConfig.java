fd2c34a07a1c6bf23f6b0aedf95ea482d31ffa00
Paweł Neścior <pawel.nescior@snowflake.com>
2022-03-03 19:59:22 +0100

    POM-66 add springdoc API documentation

---
+++ src/main/java/pl/gov/coi/pomocua/ads/configuration/ApiDocsConfig.java
@@ -0,0 +1,17 @@
+package pl.gov.coi.pomocua.ads.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("api-docs")
+public class ApiDocsConfig {
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .info(new Info().title("Documentation for ads portal"));
+    }
+}
