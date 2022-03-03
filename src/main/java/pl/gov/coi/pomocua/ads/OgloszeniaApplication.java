988ab10292a46ea504c463871852e7ed443c3022
Krzysztof Rzymkowski <krzysztof.rzymkowski@snowflake.com>
2022-03-03 18:53:43 +0100

    [POM-30] rename package ogloszenia -> ads

---
+++ src/main/java/pl/gov/coi/pomocua/ads/OgloszeniaApplication.java
@@ -0,0 +1,12 @@
+package pl.gov.coi.pomocua.ads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/gov/coi/pomocua/ads/OgloszeniaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gov.coi.pomocua.ads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
