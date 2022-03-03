dc8c851e072b08d69f5678ad19cdc056caba5c09
Hubert Lipinski <hubert.lipinski@snowflake.com>
2022-03-03 16:34:14 +0100

    [POM-42] CityLookupResource Refactor

--- src/main/java/pl/gov/coi/pomocua/ogloszenia/dictionaries/api/CityLookupResource.java
+++ src/main/java/pl/gov/coi/pomocua/ogloszenia/dictionaries/api/CityLookupResource.java
@@ -1,13 +1,12 @@
 package pl.gov.coi.pomocua.ogloszenia.dictionaries.api;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.gov.coi.pomocua.ogloszenia.dictionaries.domain.City;
 import pl.gov.coi.pomocua.ogloszenia.dictionaries.domain.CityRepository;
 
 import java.util.List;
@@ -23,17 +22,16 @@ public class CityLookupResource {
         this.cityRepository = cityRepository;
     }
 
-    private static Logger log = LoggerFactory.getLogger(CityLookupResource.class);
-
     @GetMapping(value = "/{voivodeship}/{city}")
     public ResponseEntity<CityLookupResponse> getCities(@PathVariable String voivodeship, @PathVariable String city) {
-        log.debug("getCities");
-
         if (city.isBlank() || city.length() < 2) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
 
-        List<String> cities = cityRepository.findByVoivodeshipAndCityStartsWith(voivodeship, city).stream().map(c -> c.getCity()).collect(Collectors.toList());
+        List<String> cities = cityRepository.findByVoivodeshipAndCityStartsWith(voivodeship, city)
+                .stream()
+                .map(City::getCity)
+                .collect(Collectors.toList());
 
         return new ResponseEntity<>(new CityLookupResponse(cities), HttpStatus.OK);
     }
