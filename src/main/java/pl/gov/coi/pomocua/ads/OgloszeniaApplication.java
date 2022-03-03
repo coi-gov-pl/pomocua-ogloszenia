ed4cdee35970c6e77b3e5f76e7240b0031704922
Hubert Lipinski <hubert.lipinski@snowflake.com>
2022-03-03 16:54:43 +0100

    Rename packages to ads

--- src/main/java/pl/gov/coi/pomocua/ads/OgloszeniaApplication.java
+++ src/main/java/pl/gov/coi/pomocua/ads/OgloszeniaApplication.java
@@ -1,4 +1,4 @@
-package pl.gov.coi.pomocua.ogloszenia;
+package pl.gov.coi.pomocua.ads;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
