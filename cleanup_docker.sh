078cde211bfe5aaa165e9ef7cde23d3c202277aa
Paweł Lasek <pawel.lasek@snowflake.com>
2022-03-04 12:40:05 +0100

    Renamed backend-only to db-only

--- cleanup_docker.sh
+++ cleanup_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker-compose -p help-ua --profile backend-only down -v
+docker-compose -p help-ua --profile db-only down -v
 docker rm -fv ads-pgadmin ads-postgres
 docker volume rm help-ua_pgadmin help-ua_postgres
--- start_backend_only.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec docker-compose -p help-ua --profile backend-only up --force-recreate
