bb5106ef6c37c75b042ea82c26c5db64ede323d8
Hubert Lipinski <hubert.lipinski@snowflake.com>
2022-03-05 14:55:11 +0100

    [POM-89] Fix FakeOffersCreator class

--- src/main/java/pl/gov/coi/pomocua/ads/dev/FakeOffersCreator.java
+++ src/main/java/pl/gov/coi/pomocua/ads/dev/FakeOffersCreator.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import pl.gov.coi.pomocua.ads.Location;
+import pl.gov.coi.pomocua.ads.UserId;
 import pl.gov.coi.pomocua.ads.accomodations.AccommodationOffer;
 import pl.gov.coi.pomocua.ads.accomodations.AccommodationsRepository;
 import pl.gov.coi.pomocua.ads.transport.TransportOffer;
@@ -26,12 +27,14 @@ public class FakeOffersCreator {
                 "Transport busem 8osobowy",
                 "Witam, mam busa 8 osobowego jestem wstanie pomóż w transporcie. " +
                         "Mogę też przewieź rzeczy pod granice.",
+                new UserId(1L),
                 new Location("Pomorskie", "Gdynia"), new Location("Pomorskie", "Gdynia"), 11
         );
         TransportOffer t2 = TransportOffer.of(
                 "Darmowy transport na granicę i z granicy z Ostrowa i okolic",
                 "Darmowy transport z Ostrowa i okolic na granicę z Ukraniną i z granicy " +
                         "mam 4 miejsca mam foteliki dla dzieci najleipiej w weekend",
+                new UserId(2L),
                 new Location("Pomorskie", "Gdańsk"), null, 10
         );
 
@@ -44,6 +47,7 @@ public class FakeOffersCreator {
         AccommodationOffer o1 = new AccommodationOffer();
         o1.title = "Mieszkanie w bloku, 2 osoby - Rzeszów, woj. podkarpackie";
         o1.description = "#nocleg #noclegmazowieckie #transport Dolnośląskie, miejscowość Wrocław - ok. 5 km od Dworca głównego. Kawalerka na wyłączność pomieści 2 osoby + zwierzęta są mile widziane. Okres: 2 miesiące, Bezpłatnie....";
+        o1.userId = new UserId(1L);
         o1.location = new Location("podkarpackie", "Rzeszów");
         o1.hostLanguage = List.of(AccommodationOffer.Language.PL, AccommodationOffer.Language.UA);
         o1.guests = 2;
@@ -51,6 +55,7 @@ public class FakeOffersCreator {
         AccommodationOffer o2 = new AccommodationOffer();
         o2.title = "Mieszkanie w bloku, 4 osoby - Międzygórze, woj. podlaskie";
         o2.description = "Kawalerka na wyłączność pomieści 2 osoby + zwierzęta są mile widziane. Okres: 2 miesiące, Bezpłatnie....";
+        o2.userId = new UserId(2L);
         o2.location = new Location("podlaskie", "Międzygórze");
         o2.hostLanguage = List.of(AccommodationOffer.Language.PL, AccommodationOffer.Language.UA);
         o2.guests = 4;
