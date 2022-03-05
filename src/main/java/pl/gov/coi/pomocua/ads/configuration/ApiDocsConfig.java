cd2b97e53818f897be1cedbccc423560cddc371c
Krzysztof Rzymkowski <krzysztof.rzymkowski@snowflake.com>
2022-03-05 20:04:00 +0100

    don't use http://localhost:8080 for baseUrl on openapi spec

--- src/main/java/pl/gov/coi/pomocua/ads/configuration/ApiDocsConfig.java
+++ src/main/java/pl/gov/coi/pomocua/ads/configuration/ApiDocsConfig.java
@@ -2,9 +2,12 @@ package pl.gov.coi.pomocua.ads.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.List;
 
 @Configuration
 @Profile("api-docs")
@@ -12,6 +15,7 @@ public class ApiDocsConfig {
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI()
-                .info(new Info().title("Documentation for ads portal").version("1.0"));
+                .info(new Info().title("Documentation for ads portal").version("1.0"))
+                .servers(List.of(new Server().url("/")));
     }
 }
