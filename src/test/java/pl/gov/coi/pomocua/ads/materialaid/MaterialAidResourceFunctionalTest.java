a451473c81ab2e48e0e3893a9869b60b8a2a9aac
Adam Kołodziejczyk <adam.kolodziejczyk@snowflake.com>
2022-03-06 12:35:18 +0100

    POM-95 add material aid and translations functional tests

--- src/test/java/pl/gov/coi/pomocua/ads/materialaid/MaterialAidResourceFunctionalTest.java
+++ src/test/java/pl/gov/coi/pomocua/ads/materialaid/MaterialAidResourceFunctionalTest.java
@@ -1,4 +1,4 @@
-package pl.gov.coi.pomocua.ads.jobs;
+package pl.gov.coi.pomocua.ads.materialaid;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
@@ -7,16 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.server.LocalServerPort;
 import pl.gov.coi.pomocua.ads.BaseResourceFunctionalTest;
 
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-public class JobsResourceFunctionalTest extends BaseResourceFunctionalTest {
+public class MaterialAidResourceFunctionalTest extends BaseResourceFunctionalTest {
 
   @Autowired
-  JobsRepository jobsRepository;
+  MaterialAidOfferRepository materialAidOfferRepository;
 
   @LocalServerPort
   int port;
@@ -24,17 +23,17 @@ public class JobsResourceFunctionalTest extends BaseResourceFunctionalTest {
   @BeforeEach
   public void setUp() {
     RestAssured.port = port;
-    jobsRepository.deleteAll();
+    materialAidOfferRepository.deleteAll();
   }
 
   @Override
   protected String getUrl() {
-    return "/api/jobs";
+    return "/api/material-aid";
   }
 
   @Override
   protected String postUrl() {
-    return "/api/secure/jobs";
+    return "api/secure/material-aid";
   }
 
   @Override
@@ -43,18 +42,11 @@ public class JobsResourceFunctionalTest extends BaseResourceFunctionalTest {
         {
           "title": "testTitle",
           "description": "testDescription",
-          "mode": "REMOTE",
+          "category": "FOOD",
           "location": {
             "region": "Mazowieckie",
             "city": "Warszawa"
-          },
-          "type": [
-            "TEMPORARY"
-          ],
-          "language": [
-            "UA",
-            "PL"
-          ]
+          }
         }
         """;
   }
@@ -64,11 +56,9 @@ public class JobsResourceFunctionalTest extends BaseResourceFunctionalTest {
     response
         .body("title", equalTo("testTitle"))
         .body("description", equalTo("testDescription"))
-        .body("mode", equalTo("REMOTE"))
-        .body("location.city", equalTo("Warszawa"))
+        .body("category", equalTo("FOOD"))
         .body("location.region", equalTo("Mazowieckie"))
-        .body("type", equalTo(List.of("TEMPORARY")))
-        .body("language", equalTo(List.of("UA", "PL")));
+        .body("location.city", equalTo("Warszawa"));
   }
 
   @Override
@@ -76,24 +66,20 @@ public class JobsResourceFunctionalTest extends BaseResourceFunctionalTest {
     response
         .body("content[0].title", equalTo("testTitle"))
         .body("content[0].description", equalTo("testDescription"))
-        .body("content[0].mode", equalTo("REMOTE"))
-        .body("content[0].location.city", equalTo("Warszawa"))
+        .body("content[0].category", equalTo("FOOD"))
         .body("content[0].location.region", equalTo("Mazowieckie"))
-        .body("content[0].type", equalTo(List.of("TEMPORARY")))
-        .body("content[0].language", equalTo(List.of("UA", "PL")));
+        .body("content[0].location.city", equalTo("Warszawa"));
   }
 
   @Override
   protected void assertOfferIsSavedInDb(Long id) {
-    Optional<JobOffer> jobOfferOptional = jobsRepository.findById(id);
-    assertThat(jobOfferOptional.isPresent()).isTrue();
-    JobOffer jobOffer = jobOfferOptional.get();
-    assertThat(jobOffer.title).isEqualTo("testTitle");
-    assertThat(jobOffer.description).isEqualTo("testDescription");
-    assertThat(jobOffer.mode).isEqualTo(JobOffer.Mode.REMOTE);
-    assertThat(jobOffer.location.city).isEqualTo("Warszawa");
-    assertThat(jobOffer.location.region).isEqualTo("Mazowieckie");
-    assertThat(jobOffer.type.toString()).isEqualTo("[TEMPORARY]");
-    assertThat(jobOffer.language.toString()).isEqualTo("[UA, PL]");
+    Optional<MaterialAidOffer> translationOfferOptional = materialAidOfferRepository.findById(id);
+    assertThat(translationOfferOptional.isPresent()).isTrue();
+    MaterialAidOffer materialAidOffer = translationOfferOptional.get();
+    assertThat(materialAidOffer.title).isEqualTo("testTitle");
+    assertThat(materialAidOffer.description).isEqualTo("testDescription");
+    assertThat(materialAidOffer.category).isEqualTo(MaterialAidCategory.FOOD);
+    assertThat(materialAidOffer.location.region).isEqualTo("Mazowieckie");
+    assertThat(materialAidOffer.location.city).isEqualTo("Warszawa");
   }
 }
