aec2ea6778f867a38b56196629c2ac769cc4dde7
Tomasz Kaminski <tomek.kaminski@snowflake.com>
2022-03-06 15:14:54 +0100

    POM-44 Fix migration

--- src/main/resources/db/migration/V8__material_aid_offer.sql
+++ src/main/resources/db/migration/V8__material_aid_offer.sql
@@ -5,7 +5,5 @@ create table material_aid_offer (
     description     text,
     category        text,
     city            text,
-    region          text,
-
-    CONSTRAINT PK_MAO_ID PRIMARY KEY (ID)
+    region          text
 );
