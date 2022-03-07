016056c4001580441640905bce97ba58444ade4c
sfc-gh-tmalachowski <tomek.malachowski@snowflake.com>
2022-03-07 02:27:22 +0100

    use DTO on MaterialAidOffer creation

---
+++ src/main/java/pl/gov/coi/pomocua/ads/materialaid/MaterialAidOfferDefinitionDTO.java
@@ -0,0 +1,40 @@
+package pl.gov.coi.pomocua.ads.materialaid;
+
+import org.hibernate.validator.constraints.Length;
+import pl.gov.coi.pomocua.ads.Location;
+
+import javax.persistence.Enumerated;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import static javax.persistence.EnumType.STRING;
+import static pl.gov.coi.pomocua.ads.BaseOffer.ALLOWED_TEXT;
+
+public class MaterialAidOfferDefinitionDTO {
+    @NotBlank
+    @Length(max = 80)
+    @Pattern(regexp = ALLOWED_TEXT)
+    public String title;
+
+    @NotBlank
+    @Length(max = 80)
+    @Pattern(regexp = ALLOWED_TEXT)
+    public String description;
+
+    @Valid
+    @NotNull
+    public Location location;
+
+    @Enumerated(STRING)
+    @NotNull
+    public MaterialAidCategory category;
+
+    public void applyTo(MaterialAidOffer materialAidOffer) {
+        materialAidOffer.title = this.title;
+        materialAidOffer.description = this.description;
+        materialAidOffer.category = this.category;
+        materialAidOffer.location = this.location;
+    }
+}
