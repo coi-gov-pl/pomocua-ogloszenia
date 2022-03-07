af4aed63199ce831e30fe25341a9a7fccadbc9b1
Paweł Neścior <pawel.nescior@snowflake.com>
2022-03-07 11:48:20 +0100

    POM-69 remove unnecessary base_offer table and rename following scripts

--- src/main/resources/db/migration/V4__base_offer.sql
+++
@@ -1,9 +0,0 @@
-CREATE SEQUENCE hibernate_sequence START 1 INCREMENT 1;
-
-create table base_offer (
-    id          bigserial,
-    user_id     text,
-    title       text,
-    description text,
-    CONSTRAINT PK_OFFER_ID PRIMARY KEY (ID)
-);
