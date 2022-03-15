e7d1ddaca564432bc026fbd42ebc5270d52ea020
Paweł Neścior <pawel.nescior@snowflake.com>
2022-03-15 14:50:09 +0100

    Add fake mail implementation to prevent invalid health check on local environment

---
+++ src/main/java/pl/gov/coi/pomocua/ads/dev/FakeMailConfiguration.java
@@ -0,0 +1,63 @@
+package pl.gov.coi.pomocua.ads.dev;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+
+import javax.mail.internet.MimeMessage;
+import java.io.InputStream;
+
+@Configuration
+@Profile("dev")
+public class FakeMailConfiguration {
+    @Bean
+    @Primary
+    public JavaMailSender fakeJavaMailSender() {
+        return new JavaMailSender() {
+            @Override
+            public MimeMessage createMimeMessage() {
+                return null;
+            }
+
+            @Override
+            public MimeMessage createMimeMessage(InputStream contentStream) throws MailException {
+                return null;
+            }
+
+            @Override
+            public void send(MimeMessage mimeMessage) throws MailException {
+
+            }
+
+            @Override
+            public void send(MimeMessage... mimeMessages) throws MailException {
+
+            }
+
+            @Override
+            public void send(MimeMessagePreparator mimeMessagePreparator) throws MailException {
+
+            }
+
+            @Override
+            public void send(MimeMessagePreparator... mimeMessagePreparators) throws MailException {
+
+            }
+
+            @Override
+            public void send(SimpleMailMessage simpleMessage) throws MailException {
+
+            }
+
+            @Override
+            public void send(SimpleMailMessage... simpleMessages) throws MailException {
+
+            }
+        };
+    }
+}
