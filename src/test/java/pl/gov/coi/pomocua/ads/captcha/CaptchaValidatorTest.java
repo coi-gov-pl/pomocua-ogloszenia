53ee4b12eef99ce7b30b612cbc79f4a3d29811a1
Malgorzata Antoniak <malgorzata.antoniak@nask.pl>
2022-03-25 15:21:23 +0100

    POM-359 fix recaptcha tests

--- src/test/java/pl/gov/coi/pomocua/ads/captcha/CaptchaValidatorTest.java
+++ src/test/java/pl/gov/coi/pomocua/ads/captcha/CaptchaValidatorTest.java
@@ -1,41 +1,34 @@
 package pl.gov.coi.pomocua.ads.captcha;
 
 import com.google.cloud.recaptchaenterprise.v1beta1.RecaptchaEnterpriseServiceV1Beta1Client;
+import com.google.recaptchaenterprise.v1beta1.Assessment;
+import com.google.recaptchaenterprise.v1beta1.CreateAssessmentRequest;
+import com.google.recaptchaenterprise.v1beta1.TokenProperties;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.web.client.RestOperations;
-
-import java.net.URI;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class CaptchaValidatorTest {
 
     private static final String CAPTCHA_PROJECT_ID = "xxx";
-    private static final String CAPTCHA_SECRET = "xxx";
     private static final String CAPTCHA_SITE = "xxx";
     private static final String CAPTCHA_API_KEY = "xxx";
+    private static final Float CAPTCHA_ACCEPT_LEVEL = 0.4F;
 
     private CaptchaValidator captchaValidator;
 
     private CaptchaProperties captchaProperties;
-    private RestOperations restOperations;
-    private MockHttpServletRequest request;
 
     private RecaptchaEnterpriseServiceV1Beta1Client recaptchaClient;
 
     @BeforeEach
     void setUp() {
         captchaProperties = defaultCaptchaProperties();
-        request = new MockHttpServletRequest();
-        restOperations = mock(RestOperations.class);
-
+        recaptchaClient = mock(RecaptchaEnterpriseServiceV1Beta1Client.class);
         captchaValidator = new CaptchaValidator(captchaProperties, recaptchaClient);
     }
 
@@ -66,82 +59,99 @@ class CaptchaValidatorTest {
         assertThat(result).isFalse();
     }
 
-//    @Test
-//    void validate_whenCaptchaResponseIsNull_expectFalse() {
-//
-//        //given
-//        String recaptcha = "anytext";
-//        String clientIP = "127.0.0.1";
-//        request.setRemoteAddr(clientIP);
-//
-//        URI verifyUri = URI.create(String.format(RECAPTCHA_URL_TEMPLATE, CAPTCHA_SECRET, recaptcha, clientIP));
-//        when(restOperations.getForObject(verifyUri, CaptchaResponse.class)).thenReturn(null);
-//
-//        //when
-//        boolean result = captchaValidator.validate(recaptcha);
-//
-//        //then
-//        assertAll(
-//                () -> assertThat(result).isFalse(),
-//                () -> verify(restOperations, times(1)).getForObject(verifyUri, CaptchaResponse.class)
-//        );
-//    }
+    @Test
+    void validate_whenCaptchaResponseIsNull_expectFalse() {
 
-//    @Test
-//    void validate_whenCaptchaStatusNotSuccess_expectFalse() {
-//
-//        //given
-//        String recaptcha = "anytext";
-//        String clientIP = "127.0.0.1";
-//        request.setRemoteAddr(clientIP);
-//
-//        CaptchaResponse response = new CaptchaResponse();
-//        response.setSuccess(false);
-//
-//        URI verifyUri = URI.create(String.format(RECAPTCHA_URL_TEMPLATE, CAPTCHA_SECRET, recaptcha, clientIP));
-//        when(restOperations.getForObject(verifyUri, CaptchaResponse.class)).thenReturn(response);
-//
-//        //when
-//        boolean result = captchaValidator.validate(recaptcha);
-//
-//        //then
-//        assertAll(
-//                () -> assertThat(result).isFalse(),
-//                () -> verify(restOperations, times(1)).getForObject(verifyUri, CaptchaResponse.class)
-//        );
-//    }
+        //given
+        String recaptcha = "anytext";
 
-//    @Test
-//    void validate_whenCaptchaStatusIsSuccess_expectTrue() {
-//
-//        //given
-//        String recaptcha = "anytext";
-//        String clientIP = "127.0.0.1";
-//        request.setRemoteAddr(clientIP);
-//
-//        CaptchaResponse response = new CaptchaResponse();
-//        response.setSuccess(true);
-//
-//        URI verifyUri = URI.create(String.format(RECAPTCHA_URL_TEMPLATE, CAPTCHA_SECRET, recaptcha, clientIP));
-//        when(restOperations.getForObject(verifyUri, CaptchaResponse.class)).thenReturn(response);
-//
-//        //when
-//        boolean result = captchaValidator.validate(recaptcha);
-//
-//        //then
-//        assertAll(
-//                () -> assertThat(result).isTrue(),
-//                () -> verify(restOperations, times(1)).getForObject(verifyUri, CaptchaResponse.class)
-//        );
-//    }
+        when(recaptchaClient.createAssessment(any(CreateAssessmentRequest.class))).thenReturn(null);
+
+        //when
+        boolean result = captchaValidator.validate(recaptcha);
+
+        //then
+        assertAll(
+                () -> assertThat(result).isFalse(),
+                () -> verify(recaptchaClient, times(1)).createAssessment(any(CreateAssessmentRequest.class))
+        );
+    }
+
+    @Test
+    void validate_whenCaptchaStatusNotSuccess_expectFalse() {
+
+        //given
+        String recaptcha = "anytext";
+
+        Assessment response = createAssessmentResponse(false, 0);
+
+        when(recaptchaClient.createAssessment(any(CreateAssessmentRequest.class))).thenReturn(response);
+
+        //when
+        boolean result = captchaValidator.validate(recaptcha);
+
+        //then
+        assertAll(
+                () -> assertThat(result).isFalse(),
+                () -> verify(recaptchaClient, times(1)).createAssessment(any(CreateAssessmentRequest.class))
+        );
+    }
+
+    @Test
+    void validate_whenCaptchaStatusIsSuccessAndScoreAccepted_expectTrue() {
+
+        //given
+        String recaptcha = "anytext";
+
+        Assessment response = createAssessmentResponse(true, 111);
+
+        when(recaptchaClient.createAssessment(any(CreateAssessmentRequest.class))).thenReturn(response);
+
+        //when
+        boolean result = captchaValidator.validate(recaptcha);
+
+        //then
+        assertAll(
+                () -> assertThat(result).isTrue(),
+                () -> verify(recaptchaClient, times(1)).createAssessment(any(CreateAssessmentRequest.class))
+        );
+    }
+
+    @Test
+    void validate_whenCaptchaStatusIsSuccessAndScoreNotAccepted_expectFalse() {
+
+        //given
+        String recaptcha = "anytext";
+
+        Assessment response = createAssessmentResponse(true, 0.3F);
+
+        when(recaptchaClient.createAssessment(any(CreateAssessmentRequest.class))).thenReturn(response);
+
+        //when
+        boolean result = captchaValidator.validate(recaptcha);
+
+        //then
+        assertAll(
+                () -> assertThat(result).isFalse(),
+                () -> verify(recaptchaClient, times(1)).createAssessment(any(CreateAssessmentRequest.class))
+        );
+    }
 
     private CaptchaProperties defaultCaptchaProperties() {
         CaptchaProperties properties = new CaptchaProperties();
         properties.setGoogleCloudProjectId(CAPTCHA_PROJECT_ID);
         properties.setSiteKey(CAPTCHA_SITE);
         properties.setGoogleCloudApiKey(CAPTCHA_API_KEY);
+        properties.setAcceptLevel(CAPTCHA_ACCEPT_LEVEL);
         properties.setEnabled(true);
 
         return properties;
+    }
+
+    private Assessment createAssessmentResponse(boolean valid, float score) {
+        return Assessment.newBuilder()
+                .setTokenProperties(TokenProperties.newBuilder().setValid(valid).build())
+                .setScore(score)
+                .build();
     }
 }
