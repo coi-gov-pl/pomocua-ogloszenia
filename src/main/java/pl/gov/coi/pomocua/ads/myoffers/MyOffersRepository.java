3616c3c58ad4386f99831c2c5fdc609bd0a7db43
Niedzielski Marcin <marcin.niedzielski@coi.gov.pl>
2022-03-31 13:05:41 +0200

    POM-447 - Remove account

--- src/main/java/pl/gov/coi/pomocua/ads/myoffers/MyOffersRepository.java
+++ src/main/java/pl/gov/coi/pomocua/ads/myoffers/MyOffersRepository.java
@@ -2,7 +2,10 @@ package pl.gov.coi.pomocua.ads.myoffers;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import pl.gov.coi.pomocua.ads.BaseOffer;
 import pl.gov.coi.pomocua.ads.UserId;
 
@@ -13,4 +16,8 @@ public interface MyOffersRepository extends PagingAndSortingRepository<BaseOffer
     Page<BaseOffer> findAllByUserIdAndStatus(UserId userId, BaseOffer.Status status, Pageable pageable);
 
     Optional<BaseOffer> findByIdAndUserId(Long id, UserId userId);
+
+    @Modifying
+    @Query("UPDATE BaseOffer SET status = 'INACTIVE' WHERE userId = :userId")
+    void setUserOffersAsInactive(@Param("userId") UserId userId);
 }
