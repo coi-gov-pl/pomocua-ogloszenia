80d6e715a1ee3216abecd9631fe6ca14d1e81d1c
Małgorzata Antoniak <malgorzata.antoniak@coi.gov.pl>
2022-04-08 08:10:19 +0200

    POM-473 unused method removal

--- src/main/java/pl/gov/coi/pomocua/ads/dictionaries/domain/CityRepository.java
+++ src/main/java/pl/gov/coi/pomocua/ads/dictionaries/domain/CityRepository.java
@@ -1,6 +1,5 @@
 package pl.gov.coi.pomocua.ads.dictionaries.domain;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -9,9 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CityRepository extends CrudRepository<City, Long> {
-
-    @Query("SELECT c FROM City c WHERE UPPER(CONCAT(c.location.city, ', ', c.location.region)) LIKE UPPER(CONCAT(:query, '%'))")
-    List<City> findByLocationCityWithPageable(@Param("query") String query, Pageable pageable);
 
     @Query("SELECT c FROM City c WHERE UPPER(CONCAT(c.location.city, ', ', c.location.region)) LIKE UPPER(CONCAT(:query, '%'))")
     List<City> findByLocationCityWithSort(@Param("query") String query, Sort sort);
