f08641e5b8caf2d0538a4243dd18021b97f9d562
Małgorzata Antoniak <malgorzata.antoniak@coi.gov.pl>
2022-04-14 11:25:48 +0200

    POM-32 health-care offers

---
+++ src/main/java/pl/gov/coi/pomocua/ads/health/HealthCareSpecialization.java
@@ -0,0 +1,12 @@
+package pl.gov.coi.pomocua.ads.health;
+
+public enum HealthCareSpecialization {
+    ALL,
+    GENERAL,
+    PEDIATRICS,
+    PSYCHOLOGY,
+    GYNECOLOGY,
+    NURSING,
+    STOMATOLOGY,
+    MISC
+}
